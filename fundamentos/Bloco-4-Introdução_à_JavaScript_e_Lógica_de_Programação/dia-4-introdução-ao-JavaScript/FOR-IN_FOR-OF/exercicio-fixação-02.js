cc765ce5ec5718be9a766ccde343bdb20b827319
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-08-24 10:15:57 -0400

    dia 04 incompleto

---
+++ fundamentos/Bloco-4-Introdução_à_JavaScript_e_Lógica_de_Programação/dia-4-introdução-ao-JavaScript/FOR-IN_FOR-OF/exercicio-fixação-02.js
@@ -0,0 +1,16 @@
+
+/*Usando o objeto abaixo, utilize for/in e 
+imprima um console.log com as chaves e valores 
+desse objeto.*/
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (let key in car) {
+
+    console.log(car, key)
+    
+  }
