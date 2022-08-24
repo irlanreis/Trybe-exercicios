b78de9c3623a317805c54024faf8dbd3ee2ff2f0
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-08-23 23:33:01 -0400

    adiciona exemplos e exercícios de fixação

---
+++ fundamentos/Bloco-4-Introdução_à_JavaScript_e_Lógica_de_Programação/dia-4-introdução-ao-JavaScript/exercícios-fixação/01.js
@@ -0,0 +1,24 @@
+/*Crie um objeto player contendo as variáveis listadas
+ abaixo.
+
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };*/
+
+const player = {
+name:'Marta',
+
+lastName:'Silva',
+
+age:34,
+
+medals:{ golden: 2,
+         
+         silver: 3 }
+
+        }
+
+console.log(player.age);
+console.log(player.medals.silver);
+console.log(player.name + ' ' + player.lastName);
