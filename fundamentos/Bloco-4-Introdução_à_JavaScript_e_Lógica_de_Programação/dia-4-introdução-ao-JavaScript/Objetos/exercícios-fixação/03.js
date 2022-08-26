f93c3ef5dc27400b5f7fa123b5aa016c9bedbbce
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-08-25 23:04:13 -0400

    vários arquivos de exercicios

---
+++ fundamentos/Bloco-4-Introdução_à_JavaScript_e_Lógica_de_Programação/dia-4-introdução-ao-JavaScript/Objetos/exercícios-fixação/03.js
@@ -0,0 +1,23 @@
+
+/*Adicione ao objeto a chave bestInTheWorld e atribua a 
+esta chave um array contendo as datas em que a 
+jogadora Marta foi considerada a melhor do mundo.*/
+
+const player = {
+    name:'Marta',
+    lastName:'Silva',
+    age:34,
+    medals:{ golden: 2, silver: 3 }, 
+    bestInWorld:
+    [
+         2006,
+         2007, 
+         2008, 
+         2009, 
+         2010, 
+         2018
+    ]
+  
+}
+
+console.log(player.bestInWorld.length)
