bb74d3eee13560f982ae13c6a60057a897b06c55
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-10-11 19:11:25 -0400

    renomeia diretórios

---
+++ Front-End/Seção-1-JavaScript-Testes_Assíncronos-e-Callbacks/dia-03-testes-assíncronos/exemplos course/consultaCep.test.js
@@ -0,0 +1,8 @@
+
+const { consultaCep } = require('./consultarCep');
+
+describe('Testa a função consultaCep', () => {
+    test('Verifica se retorna o logradouro', async () => {
+        expect(await consultaCep('69313616')).toEqual('Rua Soldado-Polícia Militar Arineu Ferreira Lima');
+    })
+})
---
+++ Front-End/Seção-1-JavaScript-Testes_Assíncronos-e-Callbacks/dia-03-testes-assíncronos/exemplos course/consultarCep.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+const consultaCep = async (cep) => {
+    try {
+        const requisição = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const resposta = await requisição.json();
+
+        const { logradouro } = resposta;
+        console.log(logradouro);
+
+        return logradouro;
+    } catch (error) {
+        return error.message;
+    }
+}
+consultaCep('69313616');
+module.exports = { consultaCep };
