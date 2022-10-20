5cf939892d7e959c84c85ce4d6640ea5f5da5c83
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-10-20 09:02:54 -0400

    Dev: Adiciona criação do meu primeiro componente React

---
+++ Front-End/Seção-2-Introdução_ao_React/Dia-01-Hello-World-no-React/conteudos-dia-01/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class info extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='info'>Irlan Reis</h1>
+        <p>Estudo na Trybe hà dois meses e meio</p>
+      </div>
+    )
+  }
+}
+export default info;
