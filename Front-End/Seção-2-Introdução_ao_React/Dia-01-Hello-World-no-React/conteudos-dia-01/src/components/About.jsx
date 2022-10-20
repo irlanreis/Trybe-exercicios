0296ae700a76e1a5d0cf7b26f48064c3498a39a2
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-10-20 09:27:53 -0400

    exercício de fixação finish

--- Front-End/Seção-2-Introdução_ao_React/Dia-01-Hello-World-no-React/conteudos-dia-01/src/components/About.jsx
+++ Front-End/Seção-2-Introdução_ao_React/Dia-01-Hello-World-no-React/conteudos-dia-01/src/components/About.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
+
 class info extends Component {
   render() {
+
+    const lis = ['Git', 'JavaScript', 'HTML', 'PDI', 'Gestão de Tempo', 'CSS', 'Testes Unitários'];
+
     return (
       <div>
         <h1 className='info'>Irlan Reis</h1>
         <p>Estudo na Trybe hà dois meses e meio</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          {lis.map((element, index) => <li key={index}> {element} </li>)}
+        </ul>
       </div>
     )
   }
