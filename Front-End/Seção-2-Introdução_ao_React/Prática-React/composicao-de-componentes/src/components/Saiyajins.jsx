f3d9fd2b149b4c446e330dea2ad935792c5d898a
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-10-25 21:00:54 -0400

    Adiciona exercícios do dia 02 Componentes React

---
+++ Front-End/Seção-2-Introdução_ao_React/Prática-React/composicao-de-componentes/src/components/Saiyajins.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Saiyajins extends Component {
+  render() {
+    return (
+        <section>
+          <h1>
+        Saiayyins Puros
+      </h1>
+      <ul>#</ul>  
+        </section>
+      
+    )
+  }
+}
+export default Saiyajins;
