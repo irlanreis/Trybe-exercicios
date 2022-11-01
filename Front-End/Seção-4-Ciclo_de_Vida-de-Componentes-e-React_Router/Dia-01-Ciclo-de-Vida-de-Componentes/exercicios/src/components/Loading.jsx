abba38075802351b28bb8269123035be41e1861a
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-11-01 00:17:36 -0400

    Adiciona exercício de fixação

---
+++ Front-End/Seção-4-Ciclo_de_Vida-de-Componentes-e-React_Router/Dia-01-Ciclo-de-Vida-de-Componentes/exercicios/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import  '../App.css'
+
+class Loading extends Component {
+  render() {
+    return (
+      <div >Carregando...</div>
+    )
+  }
+}
+
+export default Loading;
