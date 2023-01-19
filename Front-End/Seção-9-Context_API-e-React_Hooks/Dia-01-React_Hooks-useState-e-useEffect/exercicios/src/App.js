84995f318fa54f79151a315cf77a373cf4362eda
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-01-18 23:10:01 -0400

    feat: exercício de fixação

---
+++ Front-End/Seção-9-Context_API-e-React_Hooks/Dia-01-React_Hooks-useState-e-useEffect/exercicios/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <>
+      <h1>Meu primeiro componente funcional</h1>
+      <Form />
+    </>
+  );
+}
+
+export default App;
