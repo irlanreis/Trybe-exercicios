7576dda1f82f881b488079ef22366ddf863e4858
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-03-14 16:33:35 -0400

    Feat: Exercício 07 dia 4.2

--- Back-End/Seção-04-Introdução-ao-desenvolvimento-web-com-Node.js/Dia-02-API_REST-com-Express/Exercicios-Dia-4.2/src/app.js
+++ Back-End/Seção-04-Introdução-ao-desenvolvimento-web-com-Node.js/Dia-02-API_REST-com-Express/Exercicios-Dia-4.2/src/app.js
@@ -3,6 +3,7 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
+app.use(express.json());
 
 const moviesPath = path.resolve(__dirname, './movies.json');
 
@@ -14,6 +15,8 @@ const readfile = async () => {
         console.error(`Arquivo não pôde ser lido: ${error}`);
     };
 };
+
+readfile();
 
 app.get('/movies/:id', async (req, res) => {
     try {
@@ -31,9 +34,28 @@ app.get('/movies', async (req, res) => {
         res.status(200).json(movies);
     } catch (error) {
         res.status(500).send({ message: error.message })
-    }
+    };
 });
 
-readfile();
+app.post('/movies', async (req, res) => {
+    try {
+        const { movie, price } = req.body;
+        const movies = await readfile();
+
+        const newMovie = {
+            id: movies[movies.length -1].id +1,
+            movie,
+            price,
+        };
+
+        const allMovies = JSON.stringify([...movies, newMovie])
+
+        await fs.writeFile(moviesPath, allMovies);
+        res.status(201).json(newMovie);
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    };
+});
+
 
 module.exports = app;
