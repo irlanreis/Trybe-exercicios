6cb15c59c8421fc6b545847a5d11dc3a9f152540
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-06-09 10:48:39 -0400

    Feat: Finalmente container no up!

---
+++ Back-End/Seção-06-ORM-e-Autenticação/Dia-01-ORM-Interface-de-aplicacao-com-banco-de-dados/exercicios/src/config/congfig.js
@@ -0,0 +1,27 @@
+const config = {
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql'
+};
+
+module.exports = {
+  development: config,
+  test: config,
+  production: config,
+};
+
+/*
+username: usuário de acesso ao banco de dados;
+
+password: senha de acesso ao banco de dados;
+
+database: nome do banco de dados no qual queremos conectar;
+
+host: servidor no qual estamos conectando - por ser local, 
+utilizamos o endereço IP 127.0.0.1, ou seu alias localhost;
+
+dialect: informa qual a biblioteca que o sequelize utiliza
+para se conectar ao banco de dados estamos utilizando.
+Nesse caso, mysql.*/
