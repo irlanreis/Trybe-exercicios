a089d7d65ee74d0c98da7cc5144502d2d68a3c98
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-06-13 12:35:02 -0400

    Feat: Exercício completo

--- Back-End/Seção-06-ORM-e-Autenticação/Dia-01-ORM-Interface-de-aplicacao-com-banco-de-dados/exercicios/src/app.js
+++ Back-End/Seção-06-ORM-e-Autenticação/Dia-01-ORM-Interface-de-aplicacao-com-banco-de-dados/exercicios/src/app.js
@@ -8,5 +8,8 @@ app.use(express.json());
 
 app.get('/books', bookController.getAll);
 app.get('/books/:id', bookController.getById);
+app.post('/books', bookController.create);
+app.put('/books/:id', bookController.update);
+app.delete('/books/:id', bookController.remove);
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
