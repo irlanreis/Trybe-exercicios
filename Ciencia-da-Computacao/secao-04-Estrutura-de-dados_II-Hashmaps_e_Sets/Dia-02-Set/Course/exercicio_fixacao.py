fd840085ae040b8fc29febb8b96868bb724c792d
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-10-02 11:15:06 -0400

    Feat: Exemplo dia 02 Cs

---
+++ Ciencia-da-Computacao/secao-04-Estrutura-de-dados_II-Hashmaps_e_Sets/Dia-02-Set/Course/exercicio_fixacao.py
@@ -0,0 +1,66 @@
+class Conjunto:
+    def __init__(self):
+        self.set = [False] * 1001
+        self.last_element = 0
+
+    def add(self, item):
+        if not self.set[item]:
+            self.set[item] = True
+        if item > self.last_element:
+            self.last_element = item
+
+    def __str__(self):
+        string = "{"
+
+        for index, is_in_set in enumerate(self.set):
+            if is_in_set:
+                string += str(index)
+                if index < self.last_element:
+                    string += ","
+        string += "}"
+        return string
+
+    def __contains__(self, item):
+        return self.set[item]
+
+    def inion(self, conjunto_b):
+        new_conjunto = Conjunto()
+
+        for index in range(1001):
+            if self.set[index] or conjunto_b.set[index]:
+                new_conjunto.add(index)
+
+        return new_conjunto
+
+    def intersection(self, conjunto_b):
+        new_conjunto = Conjunto()
+
+        for index in range(1001):
+            if self.set[index] and conjunto_b.set[index]:
+                new_conjunto.add(index)
+
+        return new_conjunto
+
+
+if __name__ == "__main__":
+    conj = Conjunto()
+
+    for item in [0, 10, 100, 1000]:
+        conj.add(item)
+    print(conj)
+
+    conj2 = Conjunto()
+
+    for item in [1, 2, 3]:
+        conj.add(item)
+    print(conj)
+
+    conj3 = conj.intersection(conj2)
+    print(conj3)
+
+    for i in [7, 2, 10]:
+        conj3.add(i)
+    print(conj3)
+
+    conj4 = Conjunto()
+    print(conj4)
