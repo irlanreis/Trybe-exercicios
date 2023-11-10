aba33057c4d0f4509f7eb7f9f8f16c0d8ad44b62
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-11-10 13:50:56 -0400

    Feat: Finaliza exemplos e exercício 01

---
+++ Eletiva-de-Python/secao-04-Flask/Dia-04-Aprofundando-em-Templates-com-Flask/receitas/receita.py
@@ -0,0 +1,19 @@
+from jinja2 import Environment, FileSystemLoader
+
+loader = FileSystemLoader("../receitas/templates")
+environment = Environment(loader=loader)
+
+template = environment.get_template("receita.html")
+
+data = {
+    "nome": "Pudim de leite condensado",
+    "ingredientes": ["leite condensado", "creme de leite", "ovos", "açúcar"],
+    "preparo": "Bata o leite condensado junto com o creme de leite e os \n\
+    ovos no liquidificador por 5 minutos, enquanto isso, coloque o açúcar \n\
+      na frigideira até virar caramelo, ponha o caramelo em uma forma e \n\
+      despeje a misturam em cima, coloque para gelar",
+}
+
+output = template.render(data)
+
+print(output)
